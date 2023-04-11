d7cb7b8f63de4adcf0ceb49c1e039083ae643f52
elukashova <ele.lukashova@gmail.com>
2023-04-11 15:19:01 +0200

    refactor: create task form as an organism component

---
+++ src/components/organisms/task-form/TaskForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form
+    ref="form"
+    class="w-full flex flex-col justify-center items-center"
+    @submit.prevent="addTask"
+  >
+    <text-view tag="h2" classes="text-2xl text-center" text="What would you like to do?" />
+    <div class="w-full flex flex-row justify-center items-center">
+      <task-input @update-value="saveInputValue" />
+      <button-svg type="submit" icon-name="plus-square-thin" />
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TextView from '../../atoms/text/TextView.vue';
+import TaskInput from '../../molecules/task-input/TaskInput.vue';
+import ButtonSvg from '../../atoms/button/ButtonSvg.vue';
+
+export default defineComponent({
+  name: 'TaskForm',
+  components: {
+    TextView,
+    TaskInput,
+    ButtonSvg,
+  },
+
+  data(): { value: string } {
+    return {
+      value: '',
+    };
+  },
+
+  methods: {
+    saveInputValue(value: string): void {
+      this.value = value;
+    },
+
+    addTask(): void {
+      if (this.value) {
+        this.$store.commit('addTask', this.value);
+        this.resetForm();
+      }
+    },
+
+    resetForm(): void {
+      if (this.$refs.form && this.$refs.form instanceof HTMLFormElement) {
+        this.$refs.form.reset();
+      }
+    },
+  },
+});
+</script>
